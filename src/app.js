6c8f52df7cab72ea7d10cb66b76b55f23fc2b6a8
Magda <magdalena.dusza13@gmail.com>
2017-12-02 23:57:47 +0100

    first React class

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+let React = require('react');
+
+let Hello = React.createClass({
+  displayName: 'Hello',
+  render: function(){
+    return <div>Hello {this.props.name}</div>
+  }
+});
+
+React.render(<Hello name="Magda"/>, document.body);
